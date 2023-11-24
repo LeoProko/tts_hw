33c1cf5cf71a870b06b49e51886334bf5ee4b5f4
LeoProko <leoproko126@gmail.com>
2023-11-24 21:28:04 +0300

    wip

--- src/model/fastspeech2.py
+++ src/model/fastspeech2.py
@@ -243,18 +243,19 @@ class VarianceApapter(nn.Module):
         duration_predictor_output = self.duration_predictor(x, duration_alpha)
 
         if self.training:
-            mel_output = self.length_regulator(
-                x, duration_alpha, length_target, None, max_len
-            )
 
-            pitch_predictor_output = self.pitch_predictor(x, pitch_alpha)
+            pitch_predictor_output = self.pitch_predictor(mel_output, pitch_alpha)
             pitch_emb = self.pitch_emb(
                 torch.bucketize(pitch_target.detach(), self.pitch_bins)
             )
 
-            energy_predictor_output = self.energy_predictor(x, energy_alpha)
+            energy_predictor_output = self.energy_predictor(mel_output, energy_alpha)
             energy_emb = self.energy_emb(
                 torch.bucketize(energy_target.detach(), self.energy_bins)
+            )
+
+            mel_output = self.length_regulator(
+                x + pitch_emb + energy_emb, duration_alpha, length_target, None, max_len
             )
 
             return (
